d11694e03b8a09fc43dd3181ec83f32702ed2bd0
tiagoeuzebiocordeiro <tiagoezc@hotmail.com>
2023-10-23 16:46:27 -0300

    Testes para recurso de criacao usuario create com status 201

--- src/test/java/com/tiagoezc/demoparkapi/UsuarioIT.java
+++ src/test/java/com/tiagoezc/demoparkapi/UsuarioIT.java
@@ -1,11 +1,16 @@
 package com.tiagoezc.demoparkapi;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 import org.springframework.test.web.reactive.server.WebTestClient;
+
+import com.tiagoezc.demoparkapi.web.dto.UsuarioCreateDto;
+import com.tiagoezc.demoparkapi.web.dto.UsuarioResponseDto;
 
 /*IT de Integração
  * ué, mas o teste não é de ponto a ponto? 
@@ -19,4 +24,21 @@ public class UsuarioIT {
 	@Autowired
 	WebTestClient testClient;
 
+	@Test
+	public void createUsuario_ComUsernameEPasswordValidos_RetornarUsuarioCriadoComStatus201() {
+		
+		UsuarioResponseDto responseBody = testClient.post().uri("/api/v1/usuarios").contentType(MediaType.APPLICATION_JSON)
+		.bodyValue(new UsuarioCreateDto("tody@gmail.com", "123456"))
+		.exchange().expectStatus().isCreated()
+		.expectBody(UsuarioResponseDto.class)
+		.returnResult().getResponseBody();
+		
+		org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+		org.assertj.core.api.Assertions.assertThat(responseBody.getId()).isNotNull();
+		org.assertj.core.api.Assertions.assertThat(responseBody.getUsername()).isEqualTo("tody@gmail.com").isNotNull();
+		org.assertj.core.api.Assertions.assertThat(responseBody.getRole()).isEqualTo("CLIENTE").isNotNull();
+		/*a senha nao vai ser testada pq n retornamos a senha no response*/
+	}
+	
+	
 }
